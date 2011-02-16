9e5ab0fb496f9754359c625d7b2b69f0bc80aa70
Bruno Chagas <kolorkingindustries@gmail.com>
2011-02-16 19:27:17 -0200

    Initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,3 @@
+application = 'ruta'
+repository = ''
+hosts = %w()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruta::Application.config.secret_token = '095102170345e2a9315a9fb8444fc54604a8ad4bce15a2974eeed1e992ef3ad4a78a237f79c3018330c1b7db2114010a6e57576f3ba69b2e931d00b8e46eea98'
